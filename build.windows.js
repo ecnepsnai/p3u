54a23cb76a605b5533fde4c75b5d739b56eec8cf
Ian Spence <ian@ecnepsnai.com>
2020-08-16 16:22:02 -0700

    Fix windows package

--- build.windows.js
+++ build.windows.js
@@ -1,6 +1,6 @@
 const windowsInstaller = require('electron-winstaller');
 const package = require('./package.json');
-const packager = require('./release-package.js');
+const packager = require('./build.package.js');
 
 (async function main() {
     await packager.app('win32').then(() => {
@@ -8,7 +8,7 @@ const packager = require('./release-package.js');
             appDirectory: 'package/PlayStation 3 Updater-win32-x64',
             outputDirectory: 'package/artifacts',
             title: 'PlayStation 3 Updater',
-            iconUrl: 'https://raw.githubusercontent.com/ecnepsnai/p3u/master/icons/P3U.ico',
+            iconUrl: 'https://raw.githubusercontent.com/ecnepsnai/p3u/develop/icons/P3U.ico',
             setupIcon: 'icons/P3U.ico',
             exe: 'P3U.exe',
             setupExe: 'P3U-' + package.version + '.exe',
