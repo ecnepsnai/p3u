f53f94096c9f14061834c4c9ea51f3dc8c1ec878
Ian Spence <ian@ecnepsnai.com>
2021-04-01 21:26:20 -0700

    Fix darwin build

--- build.darwin.js
+++ build.darwin.js
@@ -3,7 +3,7 @@ const packager = require('./build.package.js');
 async function build(arch) {
     await packager.app('darwin', arch);
     await packager.exec('zip', ['-r', 'P3U_macOS_' + arch + '.zip', 'PlayStation 3 Updater.app'], { cwd: 'package/PlayStation 3 Updater-darwin-' + arch});
-    await packager.exec('mv', ['package/PlayStation 3 Updater-darwin-' + arch + '/P3U_macOS_' + arch + '.zip', 'package/artifacts/Certificate-Factory_macOS_' + arch + '.zip']);
+    await packager.exec('mv', ['package/PlayStation 3 Updater-darwin-' + arch + '/P3U_macOS_' + arch + '.zip', 'package/artifacts/P3U_macOS_' + arch + '.zip']);
 }
 
 (async function main() {
